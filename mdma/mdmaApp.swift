6b7e1be7ffa9257e3facc6cb961d7e0c6ff65574
Eros Talevi <talevineto@gmail.com>
2026-03-22 00:39:13 -0300

    Initial Commit

---
+++ mdma/mdmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdmaApp.swift
+//  mdma
+//
+//  Created by Eros Talevi on 22-03-26.
+//
+
+import SwiftUI
+
+@main
+struct mdmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
